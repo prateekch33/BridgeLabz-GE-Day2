62bd096f2080f5153446343b63b7dd6a7af6937a
prateekch33 <prateekchandra27@outlook.com>
2023-11-22 00:07:16 +0530

    Employee Attendace Added

--- employee.java
+++ employee.java
@@ -1,7 +1,18 @@
 import java.util.*;
 
 class Employee {
+	public void attendance() {
+		int checkAttendance=((int)(Math.random()*10))%2;
+		if(checkAttendance==1) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("========Welcome to Employee Wage Computation Program========");
+		Employee ob=new Employee();
+		ob.attendance();
 	}
 }
